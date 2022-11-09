b6e1e754f78b1cc8e30ca99287efce7316267acd
faithNassiwa <faithnassiwa@gmail.com>
2022-11-09 12:42:00 -0500

    upload medication

--- records/urls.py
+++ records/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    #path('', views.index, name='index'),
+    path('', views.upload_meds_data, name='upload_meds_data')
 ]
--- records/views.py
+++ records/views.py
@@ -1,9 +1,47 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.db import connection
+import requests
+from bs4 import BeautifulSoup
 
 # Create your views here.
 
+
 def index(request):
     return HttpResponse("Hello, world.")
+
+
+def upload_meds_data(request):
+    base_site = 'https://www.drugs.com/top200'
+    response = requests.get(base_site)
+    if response.ok is False:
+        print('Error Status Code: ', response.status_code)
+    else:
+        html = response.content
+    # make soup object to parse the IMDb html content
+    soup = BeautifulSoup(html, 'html.parser')
+    unordered_list = soup.find('ul', class_="ddc-list-column-2")
+    rows = unordered_list.find_all('li')
+    top_200_medications = []
+
+    for row in rows:
+        # get medication names
+        name = row.text
+        top_200_medications.append(name)
+
+    cursor = connection.cursor()
+    count = 0
+    for name in top_200_medications:
+        cursor.callproc('insert_medication', (name,))
+        count += 1
+    cursor.close()
+
+    return HttpResponse("Added {} medications".format(count))
+
+
+
+
+
+
+
 
