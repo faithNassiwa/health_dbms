7bc655153bb5bc3d3c108e116cdb204b996a1995
faithNassiwa <faithnassiwa@gmail.com>
2022-11-18 13:52:51 -0500

    file structuring

--- records/apps.py
+++ records/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class RecordsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "django_archive.records"
+    name = "records"
