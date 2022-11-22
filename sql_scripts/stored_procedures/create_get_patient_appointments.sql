cdec39a6f4939580881705b07cec1e4cb2e3d673
Zachary Jacobs <105514434+zejacobs@users.noreply.github.com>
2022-11-22 13:16:54 -0500

    Update create_get_patient_appointments.sql

--- sql_scripts/stored_procedures/create_get_patient_appointments.sql
+++ sql_scripts/stored_procedures/create_get_patient_appointments.sql
@@ -1,4 +1,7 @@
 -- Stored procedure for getting a patients appointment information
+USE hospital;
+DROP procedure IF EXISTS get_patient_appointments;
+
 delimiter $$ 
 
 CREATE PROCEDURE get_patient_appointments (
@@ -8,7 +11,8 @@ BEGIN
 	SELECT a.appointment_date AS date, a.appointment_time AS time, d.last_name AS doctor,
 		vt.name AS visit_type, a.notes AS notes
 	FROM appointment AS a, doctor AS d, visit_type AS vt
-    WHERE a.patient_id = patient_id AND d.id = a.doctor_id AND a.visit_type = vt.id;
+    WHERE a.patient_id = patient_id AND d.id = a.doctor_id
+		AND a.visit_type = vt.id AND a.status = "upcoming";
 END $$
 
 delimiter ;
