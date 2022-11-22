77746d6a7462fcf435fd7068b44cffa59a6de43a
Zachary Jacobs <105514434+zejacobs@users.noreply.github.com>
2022-11-22 10:12:43 -0500

    creates get_patient_appointments stored procedure

---
+++ sql_scripts/stored_procedures/create_get_patient_appointments.sql
@@ -0,0 +1,14 @@
+-- Stored procedure for getting a patients appointment information
+delimiter $$ 
+
+CREATE PROCEDURE get_patient_appointments (
+	IN patient_id INT
+)
+BEGIN
+	SELECT a.appointment_date AS date, a.appointment_time AS time, d.last_name AS doctor,
+		vt.name AS visit_type, a.notes AS notes
+	FROM appointment AS a, doctor AS d, visit_type AS vt
+    WHERE a.patient_id = patient_id AND d.id = a.doctor_id AND a.visit_type = vt.id;
+END $$
+
+delimiter ;
