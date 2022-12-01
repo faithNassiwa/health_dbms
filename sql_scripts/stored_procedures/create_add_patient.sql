f955155bf05813021603ff3b72d11bb0ac945263
Zachary Jacobs <105514434+zejacobs@users.noreply.github.com>
2022-12-01 12:09:53 -0500

    create add_patient stored procedure

---
+++ sql_scripts/stored_procedures/create_add_patient.sql
@@ -0,0 +1,32 @@
+-- Stored procedure for adding a new patient to the database. Makes inserts on patient and address tables.
+USE hospital;
+DROP procedure IF EXISTS add_patient;
+
+delimiter $$ 
+
+CREATE PROCEDURE add_patient (
+	IN first_name VARCHAR(50),
+    IN middle_name VARCHAR(50),
+    IN last_name VARCHAR(50),
+    IN phone_number DECIMAL(10, 0),
+    IN date_of_birth DATE,
+    IN gender VARCHAR(20),
+    IN street VARCHAR(50),
+    IN city VARCHAR(50),
+    IN state VARCHAR(50),
+    IN postal_code VARCHAR(50),
+    IN country VARCHAR(50),
+    OUT success INT
+)
+BEGIN
+	SET success = 0;
+	INSERT INTO address(street, city, state, postal_code, country)
+    VALUES (street, city, state, postal_code, country);
+
+	INSERT INTO patient(first_name, middle_name, last_name, phone_number, date_of_birth, gender, address_id)
+    VALUES (first_name, middle_name, last_name, phone_number, date_of_birth, gender, LAST_INSERT_ID());
+    SET success = 1;
+    
+END $$
+
+delimiter ;
