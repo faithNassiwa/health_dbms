eca9b7ae57fe7c60022834324475ddd781d30266
Zachary Jacobs <105514434+zejacobs@users.noreply.github.com>
2022-12-01 20:05:41 -0500

    create get_patient_visits stored procedure

---
+++ sql_scripts/stored_procedures/create_get_patient_visits.sql
@@ -0,0 +1,18 @@
+-- Stored procedure for getting a patients visit information
+USE hospital;
+DROP procedure IF EXISTS get_patient_visits;
+
+delimiter $$ 
+
+CREATE PROCEDURE get_patient_visits (
+	IN patient_id INT
+)
+BEGIN
+	SELECT v.visit_date AS date, v.visit_time AS time, d.last_name AS doctor,
+		vt.name AS visit_type, v.diagnosis AS diagnosis, v.notes AS notes
+	FROM visit AS v, doctor AS d, visit_type AS vt
+    WHERE v.patient_id = patient_id AND d.id = v.doctor_id
+		AND v.visit_type_id = vt.id;
+END $$
+
+delimiter ;
