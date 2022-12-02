3e2827bd3df006dca6cf12de5d9c47de5753b8ce
Zachary Jacobs <105514434+zejacobs@users.noreply.github.com>
2022-12-01 20:17:47 -0500

    create cancel_appointment stored procedure

---
+++ sql_scripts/stored_procedures/create_cancel_appointment.sql
@@ -0,0 +1,27 @@
+-- Stored procedure for cancelling an upcoming appointment
+USE hospital;
+DROP procedure IF EXISTS cancel_appointment;
+
+delimiter $$ 
+
+CREATE PROCEDURE cancel_appointment (
+	IN appointment_id INT,
+    OUT success INT
+)
+BEGIN
+	DECLARE appointment_status VARCHAR(50);
+	SET success = 0;
+    
+    SELECT a.status INTO appointment_status
+    FROM appointment a
+    WHERE a.id = appointment_id;
+    
+	IF LOWER(appointment_status) = 'upcoming' THEN
+		DELETE FROM appointment
+		WHERE id = appointment_id;	
+		SET success = 1;
+	END IF;
+    
+END $$
+
+delimiter ;
