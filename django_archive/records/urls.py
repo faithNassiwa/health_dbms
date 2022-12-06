f72363d3ebbefdc95f73ffdac3d86041c85d2e72
faithNassiwa <faithnassiwa@gmail.com>
2022-12-05 19:58:55 -0500

    delete django folder

--- django_archive/records/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('upload_meds_data', views.upload_meds_data, name='upload_meds_data'),
-    path('visits', views.view_patients_visit_summary, name='visits')
-]
