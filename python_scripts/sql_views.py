7f0d78e38ba32a8681a4998ff8680f1241095c2e
faithNassiwa <faithnassiwa@gmail.com>
2022-12-06 12:39:50 -0500

    update to patient visit summary to view ad dataframe

--- python_scripts/sql_views.py
+++ python_scripts/sql_views.py
@@ -10,11 +10,8 @@ def view_patients_visit_summary(conn):
     patients_visits_query = """
         SELECT * FROM hospital.patient_visit_summary;
     """
-    with conn.cursor() as cursor:
-        cursor.execute(patients_visits_query)
-        result = cursor.fetchall()
-        for row in result:
-            print(row)
+    df = pd.read_sql(patients_visits_query, conn)
+    print(df)
     return None
 
 
